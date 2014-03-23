a139af4bdc447b0477d3abc3b9c965225612620f
Arryn99 <stephanieschiniou@live.co.uk>
2014-03-23 22:55:38 +0000

    trying to fix surf class & create a homography function

--- AugmentedReality/OpenGLFramework/SurfCameraFeed.cpp
+++ AugmentedReality/OpenGLFramework/SurfCameraFeed.cpp
@@ -21,8 +21,8 @@ int SurfCameraFeed::Init(string filename) {
 
 	m_Detector = SurfFeatureDetector(minHessian);
 
-	m_Detector.detect( m_MarkerObject, m_KeyPointObject );
-	m_Extractor.compute(m_MarkerObject, m_KeyPointObject, m_DescriptionObject );
+	m_Detector.detect( m_MarkerObject, m_MarkerObjectKeyPoints );
+	m_Extractor.compute(m_MarkerObject, m_MarkerObjectKeyPoints, m_MarkerObjectDescription );
 	m_Capture = VideoCapture(0);
 
 	namedWindow("Good Matches");
@@ -30,49 +30,32 @@ int SurfCameraFeed::Init(string filename) {
 	obj_corners = std::vector<Point2f>(4);
 
 	//Get the corners from the object
+	//Draw a square around the object in screen space
 	obj_corners[0] = cvPoint(0,0);
 	obj_corners[1] = cvPoint( m_MarkerObject.cols, 0 );
 	obj_corners[2] = cvPoint( m_MarkerObject.cols, m_MarkerObject.rows );
 	obj_corners[3] = cvPoint( 0, m_MarkerObject.rows );
 
-	key = 'a';
-	framecount = 0;
+	m_Key = 'a';
+	m_Framecount = 0;
 }
 
-Mat SurfCameraFeed::CalculateHomography() {
-}
-/**
-Analyses each frame of camera feed.
-@param frame - a matrix representing the camera frame
-*/
-void SurfCameraFeed::AnalyseFrame(Mat frame) {
-
-	Mat des_image, img_matches;
-	std::vector<KeyPoint> kp_image; //an array of keypoints in the image for this frame
-	std::vector<vector<DMatch > > matches;
-	std::vector<DMatch > good_matches;
-	std::vector<Point2f> obj;
-	std::vector<Point2f> scene;
-	std::vector<Point2f> scene_corners(4);
-	Mat H;
-	Mat greyScaleImage;
-
-	//convert the camera frame to a grey scale image and save it in the image variable
-	cvtColor(frame, greyScaleImage, CV_RGB2GRAY);
+Mat SurfCameraFeed::CalculateHomography(Mat grayScaleFrame) {
 
 	//use Surf detector to detect key points
-	m_Detector.detect( greyScaleImage, kp_image);
+	m_Detector.detect(grayScaleFrame, m_FrameKeyPoints);
 
 	//compute a description object (feature vectors)
-	m_Extractor.compute( greyScaleImage, kp_image, des_image);
+	m_Extractor.compute(grayScaleFrame, m_FrameKeyPoints, m_FrameDescription);
 
-	//uses flann matcher to find similar points in computed description objects
-	m_Matcher.knnMatch(m_DescriptionObject, des_image, matches, 2);
+	//Matching descriptor vectors using FLANN matcher
+	std::vector<vector<DMatch>> matches;
+	m_Matcher.knnMatch(m_MarkerObjectDescription, m_FrameDescription, matches, 2);	
 
-	/*Draw only "good" matches (i.e. whose distance is less than 2*min_dist, or a small arbitary value ( 0.02 )
-	in the event that min_dist is very small) - radiusMatch can also be used here.
-	*/
-	for(int i = 0; i < min(des_image.rows-1,(int) matches.size()); i++)
+	std::vector< DMatch > good_matches;
+
+	//Quick calculation of max and min distances between keypoints
+	for(int i = 0; i < min(m_MarkerObjectDescription.rows-1,(int) matches.size()); i++)
 	{
 		if((matches[i][0].distance < 0.6*(matches[i][1].distance)) && ((int) matches[i].size()<=2 && (int) matches[i].size()>0))
 		{
@@ -80,45 +63,70 @@ void SurfCameraFeed::AnalyseFrame(Mat frame) {
 		}
 	}
 
+	//Localize the object
+	std::vector<Point2f> obj;
+	std::vector<Point2f> scene;
+
 	//Draw only "good" matches
-	drawMatches( m_MarkerObject, m_KeyPointObject, greyScaleImage, kp_image, good_matches, img_matches, Scalar::all(-1), Scalar::all(-1), vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+	//drawMatches( m_MarkerObject, m_MarkerObjectKeyPoints, greyScaleImage, m_FrameKeyPoints, good_matches, img_matches, Scalar::all(-1), Scalar::all(-1), vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
 
 	if (good_matches.size() >= 4)
 	{
 		for( int i = 0; i < good_matches.size(); i++ )
 		{
 			//Get the keypoints from the good matches
-			obj.push_back( m_KeyPointObject[ good_matches[i].queryIdx ].pt );
-			scene.push_back( kp_image[ good_matches[i].trainIdx ].pt );
+			obj.push_back( m_MarkerObjectKeyPoints[ good_matches[i].queryIdx ].pt );
+			scene.push_back( m_FrameKeyPoints[ good_matches[i].trainIdx ].pt );
 		}
-
-		H = findHomography( obj, scene, CV_RANSAC );
-
-		perspectiveTransform( obj_corners, scene_corners, H);
-
-		//Draw lines between the corners (the mapped object in the scene image )
-		line( img_matches, scene_corners[0] + Point2f( m_MarkerObject.cols, 0), scene_corners[1] + Point2f( m_MarkerObject.cols, 0), Scalar(0, 255, 0), 4 );
-		line( img_matches, scene_corners[1] + Point2f( m_MarkerObject.cols, 0), scene_corners[2] + Point2f( m_MarkerObject.cols, 0), Scalar( 0, 255, 0), 4 );
-		line( img_matches, scene_corners[2] + Point2f( m_MarkerObject.cols, 0), scene_corners[3] + Point2f( m_MarkerObject.cols, 0), Scalar( 0, 255, 0), 4 );
-		line( img_matches, scene_corners[3] + Point2f( m_MarkerObject.cols, 0), scene_corners[0] + Point2f( m_MarkerObject.cols, 0), Scalar( 0, 255, 0), 4 );
+		return findHomography(obj, scene, CV_RANSAC);
 	}
-	//Show detected matches
-	imshow( "Good Matches", img_matches );
-
+	return Mat();
 }
+
+/**
+Analyses each frame of camera feed.
+@param frame - a matrix representing the camera frame
+*/
+void SurfCameraFeed::AnalyseFrame(Mat frame) {
+
+	Mat frameDescription;
+	std::vector<KeyPoint> frameKeyPoints; //an array of keypoints in the image for this frame
+	Mat greyScaleImage;
+
+	//convert the camera frame to a grey scale image and save it in the image variable
+	cvtColor(frame, greyScaleImage, CV_RGB2GRAY);
+
+	Mat H = CalculateHomography(greyScaleImage);
+
+	std::vector<Point2f> sceneCorners(4);
+
+	if (H.cols > 0) {
+	perspectiveTransform( obj_corners, sceneCorners, H);
+	}
+
+	//Draw lines between the corners (the mapped object in the scene image )
+	line( frame, sceneCorners[0] + Point2f( m_MarkerObject.cols, 0), sceneCorners[1] + Point2f( m_MarkerObject.cols, 0), Scalar(0, 255, 0), 4 );
+	line( frame, sceneCorners[1] + Point2f( m_MarkerObject.cols, 0), sceneCorners[2] + Point2f( m_MarkerObject.cols, 0), Scalar( 0, 255, 0), 4 );
+	line( frame, sceneCorners[2] + Point2f( m_MarkerObject.cols, 0), sceneCorners[3] + Point2f( m_MarkerObject.cols, 0), Scalar( 0, 255, 0), 4 );
+	line( frame, sceneCorners[3] + Point2f( m_MarkerObject.cols, 0), sceneCorners[0] + Point2f( m_MarkerObject.cols, 0), Scalar( 0, 255, 0), 4 );
+	
+	//Show detected matches
+	imshow("Good Matches", frame);
+}
+
 int SurfCameraFeed::Frame() {
 
 	Mat frame;
 	m_Capture >> frame;
 
-	if (framecount < 5)
+	if (m_Framecount < 5)
 	{
-		framecount++;
+		m_Framecount++;
 		return 0;
 	}
 
 	AnalyseFrame(frame);
 
-	key = waitKey(1);
+	m_Key = waitKey(1);
 	return 0;
 }
--- AugmentedReality/OpenGLFramework/SurfCameraFeed.h
+++ AugmentedReality/OpenGLFramework/SurfCameraFeed.h
@@ -22,24 +22,39 @@ public:
 	int Init(string);
 	int Frame();
 
-	//analyses a camera frame and returns a matrix representing a markers position. 
+	//analyses a camera frame and returns a matrix representing a marker's position. 
 	void AnalyseFrame(Mat frame);
-	Mat CalculateHomography(Mat );
+
+	//calculates a homography matrix from m_MarkerObject & grayScaleFrame
+	Mat CalculateHomography(Mat grayScaleFrame);
 
 private:
-	Mat m_MarkerObject;
-	SurfFeatureDetector m_Detector;
-	std::vector<KeyPoint> m_KeyPointObject;
-	SurfDescriptorExtractor m_Extractor;
-    Mat m_DescriptionObject;
-	FlannBasedMatcher m_Matcher;
-
 	VideoCapture m_Capture;
 
+	/*=========== OpenCV utility objects ============*/
+
+	SurfFeatureDetector m_Detector;
+	SurfDescriptorExtractor m_Extractor;
+	FlannBasedMatcher m_Matcher;
+
+	/*=========== Variables calculated during init function ============*/
+
+	//the image we are looking for
+	Mat m_MarkerObject;
+	//key points extracted during init function for marker object
+	std::vector<KeyPoint> m_MarkerObjectKeyPoints;
+	//description object for m_MarkerObject (feature vector) calculated during init function
+	Mat m_MarkerObjectDescription;
+
+	/*=========== Variables calculated during update function ============*/
+	Mat m_FrameDescription;
+	std::vector<KeyPoint> m_FrameKeyPoints; //an array of keypoints in the image for this frame
+
+	//array of 2D points representing corners of image
 	std::vector<Point2f> obj_corners;
 
-	char key;
-    int framecount;
+	char m_Key;
+    int m_Framecount;
 };
 
 #endif
