c9bcf436b4b869251d22695e09c78ffa468e128b
Arryn99 <stephanieschiniou@live.co.uk>
2014-04-27 14:44:36 +0100

    changes from last weekend of working

--- AugmentedReality/OpenGLFramework/main.cpp
+++ AugmentedReality/OpenGLFramework/main.cpp
@@ -1,35 +1,24 @@
-#include "SurfCameraFeed.h"
 #include <windows.h>
 #include "systemclass.h"
+
 
 using namespace cv;
 using namespace std;
 
-SurfCameraFeed surfCameraInstance;
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
-{
-	surfCameraInstance.Init("Photo2.jpg");
-
-	while (true) {
-		surfCameraInstance.Frame();
-	}
-
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow) {
 	SystemClass* System;
 	bool result;
 	//int counter = 0;
 	
 	// Create the system object.
 	System = new SystemClass;
-	if(!System)
-	{
+	if(!System) {
 		return 0;
 	}
 
 	// Initialize and run the system object.
 	result = System->Initialize();
-	if(result)
-	{
+	if(result) {
 		System->Run();
 	}
 
